47e9d27c6471e8d6007859306a37162866e9bfd6
Kevin321an <kevin321an@gmail.com>
2016-07-17 22:47:12 -0400

    complete

--- app/scripts/controllers/bricks-a-bunch.js
+++ app/scripts/controllers/bricks-a-bunch.js
@@ -8,21 +8,13 @@
  * Controller of the serviceQuizApp
  */
 angular.module('serviceQuizApp')
-  .controller('BricksABunchCtrl', function () {
+  .controller('BricksABunchCtrl', ['brickWarehouse', function (warehouse) {
     this.name = 'Bricks A Bunch';
+    this.redBricks = warehouse.bricks.red;
 
-    this.redBricks = {
-      '1x1': {
-        quantity: 13,
-        price: 0.01
-      },
-      '2x2': {
-        quantity: 48,
-        price: 0.03
-      },
-      '2x6': {
-        quantity: 9,
-        price: 0.05
-      }
+    this.addToCart = function(size){
+      warehouse.decreaseQuantity('red', size);
     };
-  });
+
+    //this.redBricks = brickWarehouse.bricks.red;
+  }]);
